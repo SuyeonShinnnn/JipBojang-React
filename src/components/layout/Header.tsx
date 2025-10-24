fa2c0119bee73560624cd3a6e8ff2c2bb82357e5
SuyeonShinnnn <suyeons5536@gmail.com>
2025-10-24 21:33:23 +0900

    Refactor: module.css 에서 css-in-js로 변경 (#4)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import style from './header.module.css';
+import styled from 'styled-components';
 
 const Header: React.FC = () => {
   type NavItems = {
@@ -15,24 +15,67 @@ const Header: React.FC = () => {
     { name: '전문가 상담', path: '/consult' },
     { name: '커뮤니티', path: '/community' },
   ]);
+
   return (
-    <header>
-      <div className="d-flex justify-content-between px-5 py-2">
-        <img src="/logo.svg" />
-        <div className={`${style.pageLink} gap-5 align-items-center`}>
+    <HeaderContainer>
+      <ItemWrapper>
+        <LogoImage src="/logo.svg" />
+        <NavItemsWrapper>
           {navItems.map((item, index) => (
             <Link key={index} to={item.path}>
               {item.name}
             </Link>
           ))}
-        </div>
-        <div className={`${style.memberLink} align-items-center`}>
+        </NavItemsWrapper>
+        <UserNavItemsWrapper>
           <Link to="/login">로그인</Link>
           <Link to="/signUp">회원가입</Link>
-        </div>
-      </div>
-    </header>
+        </UserNavItemsWrapper>
+      </ItemWrapper>
+    </HeaderContainer>
   );
 };
+
+const HeaderContainer = styled.header`
+  background-color: white;
+  width: 100%;
+  padding: 0.5rem 0;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+
+  display: flex;
+  justify-content: center;
+`;
+
+const ItemWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 95%;
+`;
+
+const LogoImage = styled.img`
+  width: 80px;
+`;
+
+const NavItemsWrapper = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 3rem;
+  font-size: 20px;
+`;
+
+const UserNavItemsWrapper = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1.5rem;
+  font-size: 16px;
+`;
 
 export default Header;
