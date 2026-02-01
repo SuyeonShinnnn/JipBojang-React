22b2ca10ebeaa28bda77463dafb0ae9b5a04ac7c
SuyeonShinnnn <suyeons5536@gmail.com>
2026-02-01 20:35:00 +0900

    Feat: 주소 검색 섹션 컴포넌트 분리 및 다음 모달 섹션 이동 기능 구현 (#10)

---
+++ src/pages/notification/components/AddressSection.tsx
@@ -0,0 +1,137 @@
+import BaseInput from '../../../components/common/BaseInput';
+import styled from 'styled-components';
+import { useQuery } from '@tanstack/react-query';
+import { searchAddress } from '../../../apis/notiApi';
+import { DotLoader } from 'react-spinners';
+import { useState } from 'react';
+
+interface Address {
+  type: string;
+  address: string;
+  uniqueNo: string;
+}
+
+const AddressSection: React.FC = () => {
+  const [keyword, setKeyword] = useState('');
+  const [selectedAddress, setSelectedAddress] = useState<Address | null>(null);
+  const [searched, setSearched] = useState(false);
+
+  const { data, isPending, isSuccess, isError, refetch } = useQuery<Address[]>({
+    queryKey: ['addressSearch', keyword],
+    queryFn: () => searchAddress(keyword),
+    enabled: false,
+  });
+
+  const handleSearch = () => {
+    setSearched(true);
+    console.log(searched);
+    refetch();
+  };
+
+  return (
+    <>
+      <Title>주소 검색</Title>
+      <BaseInput
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+        showButton={true}
+        onButtonClick={handleSearch}
+      />
+      <ResultBox $center={searched && isPending}>
+        {!searched && (
+          <>
+            <span>
+              * 부동산의 소재지번 또는 도로면, 동/호로 검색하시면 정확한 검색
+              결과를 얻을 수 있습니다.
+            </span>
+            <div>
+              <span>(예) 남부순환로 2803 101동 101호</span>
+              <span>도곡동 91-5 101동 101호</span>
+            </div>
+
+            <span>* 미등기인 경우 주소 조회 및 등록이 되지 않습니다.</span>
+          </>
+        )}
+        {searched && isPending && (
+          <>
+            <DotLoader
+              color="var(--color-primary)"
+              loading={isPending}
+              size={52}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+            <LoadingSpan>검색중</LoadingSpan>
+          </>
+        )}
+        {searched && isSuccess && (
+          <ResultUL>
+            {data.map((addr) => (
+              <ResultList
+                key={addr.uniqueNo}
+                tabIndex={0}
+                onClick={() => setSelectedAddress(addr)}
+              >
+                <TypeSpan>{addr.type}</TypeSpan>
+                <span>{addr.uniqueNo}</span>
+                <span>{addr.address}</span>
+              </ResultList>
+            ))}
+          </ResultUL>
+        )}
+        {searched && isError && <>error</>}
+      </ResultBox>
+    </>
+  );
+};
+
+export default AddressSection;
+
+const Title = styled.h3`
+  margin-bottom: 8px;
+`;
+
+const ResultBox = styled.div<{ $center?: boolean }>`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  height: 240px;
+
+  ${({ $center }) => $center && `justify-content: center; align-items:center`}
+`;
+
+const LoadingSpan = styled.span`
+  margin-top: 12px;
+  color: var(--color-darkgray);
+`;
+
+const ResultUL = styled.ul`
+  gap: 8px;
+  margin: 0;
+  padding: 0;
+  overflow-y: auto;
+`;
+
+const ResultList = styled.li`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  padding: 12px;
+  border: 1px solid transparent;
+  border-radius: 8px;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #f5f5f5;
+  }
+  &:focus {
+    outline: none;
+    background-color: #f0f1ff;
+    border: 1px solid var(--color-primary);
+  }
+`;
+
+const TypeSpan = styled.span`
+  color: var(--color-primary);
+`;
--- src/pages/notification/components/PropertyRegistModal.tsx
+++ src/pages/notification/components/PropertyRegistModal.tsx
@@ -1,139 +1,58 @@
 import React, { useState } from 'react';
 import BaseModal from '../../../components/common/BaseModal';
-import BaseInput from '../../../components/common/BaseInput';
-import styled from 'styled-components';
-import { useQuery } from '@tanstack/react-query';
-import { searchAddress } from '../../../apis/notiApi';
-import { DotLoader } from 'react-spinners';
+import BaseButton from '../../../components/common/BaseButton';
+import AddressSection from './AddressSection';
 
 interface PropertyRegistModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
-interface Address {
-  type: string;
-  address: string;
-  uniqueNo: string;
-}
+type Section = 'Address' | 'Period' | 'Check';
+const sections: Section[] = ['Address', 'Period', 'Check'];
 
 const PropertyRegistModal: React.FC<PropertyRegistModalProps> = ({
   isOpen,
   onClose,
 }) => {
-  const [keyword, setKeyword] = useState('');
-  const [searched, setSearched] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentSection = sections[currentIndex];
 
-  const { data, isPending, isSuccess, isError, refetch } = useQuery<Address[]>({
-    queryKey: ['addressSearch', keyword],
-    queryFn: () => searchAddress(keyword),
-    enabled: false,
-  });
+  const nextSection = () => {
+    if (currentIndex < sections.length - 1) setCurrentIndex((prev) => prev + 1);
+  };
 
-  const handleSearch = () => {
-    setSearched(true);
-    console.log(searched);
-    refetch();
+  const prevSection = () => {
+    if (currentIndex > 0) setCurrentIndex((prev) => prev - 1);
+  };
+
+  const closeClicked = () => {
+    onClose;
   };
 
   return (
-    <BaseModal isOpen={isOpen} onClose={onClose} header={<h2>부동산 등록</h2>}>
-      <Title>주소 검색</Title>
-      <BaseInput
-        value={keyword}
-        onChange={(e) => setKeyword(e.target.value)}
-        showButton={true}
-        onButtonClick={handleSearch}
-      />
-      <ResultBox $center={searched && isPending}>
-        {!searched && (
-          <>
-            <span>
-              * 부동산의 소재지번 또는 도로면, 동/호로 검색하시면 정확한 검색
-              결과를 얻을 수 있습니다.
-            </span>
-            <div>
-              <span>(예) 남부순환로 2803 101동 101호</span>
-              <span>도곡동 91-5 101동 101호</span>
-            </div>
-
-            <span>* 미등기인 경우 주소 조회 및 등록이 되지 않습니다.</span>
-          </>
-        )}
-        {searched && isPending && (
-          <>
-            <DotLoader
-              color="var(--color-primary)"
-              loading={isPending}
-              size={52}
-              aria-label="Loading Spinner"
-              data-testid="loader"
-            />
-            <LoadingSpan>검색중</LoadingSpan>
-          </>
-        )}
-        {searched && isSuccess && (
-          <ResultUL>
-            {data.map((addr) => (
-              <ResultList key={addr.uniqueNo}>
-                <TypeSpan>{addr.type}</TypeSpan>
-                <span>{addr.uniqueNo}</span>
-                <span>{addr.address}</span>
-              </ResultList>
-            ))}
-          </ResultUL>
-        )}
-        {searched && isError && <>error</>}
-      </ResultBox>
+    <BaseModal
+      isOpen={isOpen}
+      onClose={onClose}
+      header={<h2>부동산 등록</h2>}
+      footer={
+        <>
+          <BaseButton
+            variant="outline"
+            onClick={() => {
+              setCurrentIndex(0);
+              onClose();
+            }}
+          >
+            취소
+          </BaseButton>
+          <BaseButton onClick={nextSection}>다음</BaseButton>
+        </>
+      }
+    >
+      {currentSection === 'Address' && <AddressSection />}
     </BaseModal>
   );
 };
 
 export default PropertyRegistModal;
-
-const Title = styled.h3`
-  margin-bottom: 8px;
-`;
-
-const ResultBox = styled.div<{ $center?: boolean }>`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-  height: 240px;
-
-  ${({ $center }) => $center && `justify-content: center; align-items:center`}
-`;
-
-const LoadingSpan = styled.span`
-  margin-top: 12px;
-  color: var(--color-darkgray);
-`;
-
-const ResultUL = styled.ul`
-  /* display: flex;
-  flex-direction: column;
-  gap: 8px; */
-  margin: 0;
-  padding: 0;
-  overflow-y: auto;
-`;
-
-const ResultList = styled.li`
-  /* background-color: aliceblue; */
-  list-style: none;
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
-  padding: 12px;
-  border-radius: 8px;
-
-  &:hover {
-    cursor: pointer;
-    background-color: #f5f5f5;
-  }
- 
-`;
-
-const TypeSpan = styled.span`
-  color: var(--color-primary);
-`;
