653fd50f321441cdf19bf20b919f7d69ae972d7f
SuyeonShinnnn <suyeons5536@gmail.com>
2026-02-06 15:05:58 +0900

    Feat: NotFoundPage 구현 (#13)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import HomePage from './pages/HomePage';
 import ReportPage from './pages/ReportPage';
 import NotiPage from './pages/notification/NotiPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/report" element={<ReportPage />} />
             <Route path="/notify" element={<NotiPage />} />
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </main>
       </Router>
