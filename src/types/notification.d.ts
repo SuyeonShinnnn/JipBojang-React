49659368c541e2aa83f87149fdf7827d1663f711
SuyeonShinnnn <suyeons5536@gmail.com>
2026-03-09 19:17:09 +0900

    Fix: 파일 이동으로인한 타입 누락 문제 해결 (#16)

--- src/types/notification.d.ts
+++ src/types/notification.d.ts
@@ -3,3 +3,20 @@ export interface AddressInfo {
   address: string;
   uniqueNo: string;
 }
+
+export interface PropertyDetail {
+  commAddrLotNumber: string;
+  commUniqueNo: string;
+  expiredDate: string;
+  id: number;
+  registDate: string;
+  resType: string;
+  title: string;
+  userId: number;
+}
+
+export interface RegistryChanged {
+  id: number;
+  changedDate: string;
+  detail: string;
+}
