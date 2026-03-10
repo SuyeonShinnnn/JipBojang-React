ca383767f5a5a41fb404583fed4463b9226423a6
SuyeonShinnnn <suyeons5536@gmail.com>
2026-03-10 19:48:59 +0900

    Feat: 검색 기록 저장 기능 구현 (#16)

--- src/pages/building/components/SearchBar.tsx
+++ src/pages/building/components/SearchBar.tsx
@@ -1,8 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BaseInput from '../../../components/common/BaseInput';
 import styled from 'styled-components';
 import type { Place } from '../../../types/building';
-import { houseLabel } from '../../../utils/BuildingUtils';
+import {
+  getRecentSearch,
+  removeRecentSearch,
+  saveRecentSearch,
+} from '../../../utils/BuildingUtils';
+import { RiCloseLargeFill } from 'react-icons/ri';
 
 interface SearchBarProps {
   places: Place[];
@@ -11,37 +16,81 @@ interface SearchBarProps {
 }
 
 const SearchBar = ({ onSearch, onSelect, places }: SearchBarProps) => {
+  const [recentSearch, setRecentSearch] = useState<string[]>([]);
   const [searchKeyword, setSearchKeyword] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
     if (!searchKeyword.trim()) return;
     onSearch(searchKeyword);
+    saveRecentSearch(searchKeyword);
+    setRecentSearch(getRecentSearch());
+    (e.target as HTMLFormElement).querySelector('input')?.blur();
   };
 
+  useEffect(() => {
+    setRecentSearch(getRecentSearch());
+  }, []);
+
   return (
-    <Aside hasResult={places.length > 0}>
-      <form onSubmit={handleSubmit}>
-        <BaseInput
-          showButton={true}
-          placeholder="검색어를 입력하세요"
-          value={searchKeyword}
-          onChange={(e) => setSearchKeyword(e.target.value)}
-        />
-      </form>
-      <ResultSection>
-        <ul>
-          {places.map((item, key) => (
-            <List key={key} onClick={() => onSelect(item)}>
-              <TitleWrapper>
-                <span>{item.name}</span>
-              </TitleWrapper>
-              <Address>{item.address}</Address>
-            </List>
-          ))}
-        </ul>
-      </ResultSection>
-    </Aside>
+    <>
+      <Aside hasResult={places.length > 0}>
+        <form onSubmit={handleSubmit}>
+          <BaseInput
+            showButton={true}
+            placeholder="검색어를 입력하세요"
+            value={searchKeyword}
+            onChange={(e) => setSearchKeyword(e.target.value)}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+          />
+        </form>
+
+        <ResultSection>
+          <ul>
+            {places.map((item, key) => (
+              <List key={key} onClick={() => onSelect(item)}>
+                <TitleWrapper>
+                  <span>{item.name}</span>
+                </TitleWrapper>
+                <Address>{item.address}</Address>
+              </List>
+            ))}
+          </ul>
+        </ResultSection>
+      </Aside>
+      {isFocused && (
+        <RecentSearchBox>
+          <h5>최근 검색어</h5>
+          {recentSearch.length !== 0 ? (
+            recentSearch.map((item, key) => (
+              <li
+                key={key}
+                onMouseDown={() => {
+                  setSearchKeyword(item);
+                  onSearch(item);
+                }}
+              >
+                {item}
+                <RiCloseLargeFill
+                  onMouseDown={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    removeRecentSearch(item);
+                  }}
+                />
+              </li>
+            ))
+          ) : (
+            <EmptyBox>
+              <span>검색 기록이 없습니다.</span>
+            </EmptyBox>
+          )}
+        </RecentSearchBox>
+      )}
+    </>
   );
 };
 
@@ -64,6 +113,44 @@ const Aside = styled.aside<{ hasResult: boolean }>`
   flex-direction: column;
 
   transition: height 0.5s ease;
+`;
+
+const RecentSearchBox = styled.ul`
+  position: absolute;
+  top: 9.8rem;
+  left: 1.7rem;
+  width: 332px;
+  background: white;
+  border-radius: 8px;
+  box-shadow: 0 8px 12px rgba(0, 0, 0, 0.1);
+  padding-bottom: 1rem;
+  z-index: 2000;
+
+  h5 {
+    font-size: 16px;
+    margin-bottom: 12px;
+    padding: 1rem 0 0 1rem;
+  }
+  li {
+    display: flex;
+    justify-content: space-between;
+    padding: 12px;
+
+    svg {
+      color: var(--color-darkgray);
+    }
+
+    &:hover {
+      cursor: pointer;
+      background-color: #f5f5f5;
+    }
+  }
+`;
+
+const EmptyBox = styled.div`
+  text-align: center;
+  padding: 2rem 0;
+  color: var(--color-darkgray);
 `;
 
 const ResultSection = styled.section`
--- src/utils/BuildingUtils.ts
+++ src/utils/BuildingUtils.ts
@@ -7,10 +7,40 @@ export const houseLabel = (categoryName: string) => {
 
 export const dateFormat = (date?: string) => {
   if (date?.length != 8) return '';
-  
+
   const year = date.slice(0, 4);
   const month = date.slice(4, 6);
   const day = date.slice(6, 8);
 
   return `${year}년 ${month}월 ${day}일`;
+};
+
+// 최근 검색 기록 저장
+export const saveRecentSearch = (keyword: string) => {
+  const stored = localStorage.getItem('recentSearch');
+
+  let list: string[] = stored ? JSON.parse(stored) : [];
+
+  list = list.filter((item) => item !== keyword); // 중복 제거
+  list.unshift(keyword); // 맨 앞 추가
+  list = list.slice(0, 5); // 최대 5개
+
+  localStorage.setItem('recentSearch', JSON.stringify(list));
+};
+
+// 검색 기록 가져오기
+export const getRecentSearch = () => {
+  const stored = localStorage.getItem('recentSearch');
+  return stored ? JSON.parse(stored) : [];
+};
+
+// 검색 기록 삭제
+export const removeRecentSearch = (keyword: string) => {
+  const stored = JSON.parse(localStorage.getItem('recentSearch') || '[]');
+
+  const updated = stored.filter((item: string) => item !== keyword);
+
+  localStorage.setItem('recentSearch', JSON.stringify(updated));
+
+  return updated;
 };
