0c4374969bfd6175dd4f48125d6c5a8b3c9f157a
SuyeonShinnnn <suyeons5536@gmail.com>
2026-03-10 22:07:23 +0900

    Feat: 플로팅 버튼 제작 (#19)

--- src/components/layout/DefaultLayout.tsx
+++ src/components/layout/DefaultLayout.tsx
@@ -6,22 +6,30 @@ import NotFoundPage from '../../pages/NotFoundPage';
 import LoginPage from '../../pages/auth/LoginPage';
 import styled, { keyframes } from 'styled-components';
 import BuildingPage from '../../pages/building/BuildingPage';
+import chatbotIcon from '../../assets/chatbot/chatbot.png';
 
 const DefaultLayout = () => {
   const location = useLocation();
   const isBuildingPage = location.pathname === '/building';
 
   return (
-    <MainContent key={location.pathname} $noPadding={isBuildingPage}>
-      <Routes location={location}>
-        <Route path="login" element={<LoginPage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/report" element={<ReportPage />} />
-        <Route path="/notify" element={<NotiPage />} />
-        <Route path="/building" element={<BuildingPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </MainContent>
+    <>
+      <MainContent key={location.pathname} $noPadding={isBuildingPage}>
+        <Routes location={location}>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/report" element={<ReportPage />} />
+          <Route path="/notify" element={<NotiPage />} />
+          <Route path="/building" element={<BuildingPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </MainContent>
+      
+      <ChatbotButton>
+        <Image src={chatbotIcon} />
+        <span>챗봇</span>
+      </ChatbotButton>
+    </>
   );
 };
 
@@ -41,4 +49,32 @@ const fadeSlideIn = keyframes`
 const MainContent = styled.main<{ $noPadding?: boolean }>`
   padding-top: ${({ $noPadding }) => ($noPadding ? '0' : '4.5rem')};
   animation: ${fadeSlideIn} 0.35s ease-out both;
+`;
+
+const ChatbotButton = styled.button`
+  position: fixed;
+  right: 1.3rem;
+  bottom: 1.3rem;
+
+  width: 92px;
+  height: 92px;
+  color: white;
+  background-color: var(--color-primary);
+  box-shadow: 5px 5px 20px var(--color-mediumgray);
+  border-style: none;
+  border-radius: 50px;
+  padding: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Image = styled.img`
+  width: 48px;
 `;
