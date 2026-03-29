a69c82feebd00d37f8d5375941b2b62f760c7735
SuyeonShinnnn <suyeons5536@gmail.com>
2026-03-29 20:45:44 +0900

    Feat: 개요 섹션 UI 구현 (#25)

--- src/pages/report/ReportResultPage.tsx
+++ src/pages/report/ReportResultPage.tsx
@@ -1,5 +1,17 @@
+import styled from 'styled-components';
+import OverviewSection from './components/OverviewSection';
+
 const ReportResultPage = () => {
-  return <></>;
+  return (
+    <Main>
+      <OverviewSection />
+    </Main>
+  );
 };
 
 export default ReportResultPage;
+
+const Main = styled.main`
+  display: flex;
+  justify-content: center;
+`;
