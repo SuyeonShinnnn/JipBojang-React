88c03d50e5b8509c1a7084238534563dc9112a30
SuyeonShinnnn <suyeons5536@gmail.com>
2026-04-06 16:48:09 +0900

    Refactor: 로그인 요청에 공통 axiosInstance 적용 (#28)

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -69,7 +69,7 @@ export const useAuthStore = create<AuthState>((set, get) => ({
   // 일반 로그인
   login: async ({ id, password }) => {
     try {
-      const { data } = await axios.post('/api/users/login', {
+      const { data } = await axiosInstance.post('/api/users/login', {
         id,
         password,
       });
