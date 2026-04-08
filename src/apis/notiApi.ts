7f3456097c1a13d75c3b4ef907ab482d5f9b75bf
SuyeonShinnnn <suyeons5536@gmail.com>
2026-04-09 00:10:03 +0900

    Fix: 알림 등록 부동산 조회 API 경로 수정 (#32)

--- src/apis/notiApi.ts
+++ src/apis/notiApi.ts
@@ -3,7 +3,9 @@ import axiosInstance from './axiosInstance';
 
 export const getRegistedPropertyInfo = async (id: number) => {
   try {
-    const response = await axios.get(`/api/notification/regist/${id}`);
+    const response = await axiosInstance.get(
+      `/api/notification/registed/${id}`,
+    );
     return response.data;
   } catch (e) {
     console.log('🚨오류 발생');
