8978a09e10ed38a36056a93513d9df4f758a3c19
SuyeonShinnnn <suyeons5536@gmail.com>
2026-04-10 22:27:39 +0900

    Design: 모달 Overlay height 조정 (#32)

--- src/components/common/BaseModal.tsx
+++ src/components/common/BaseModal.tsx
@@ -66,7 +66,7 @@ const Overlay = styled.div<{ $isOpen?: boolean }>`
   top: 0;
   left: 0;
   width: 100%;
-  height: 100%;
+  height: 100vh;
   background-color: rgba(0, 0, 0, 0.25);
   display: flex;
   align-items: center;
