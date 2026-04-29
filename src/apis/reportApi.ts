a47237fd274c508c6054c7ba3df4b25a57a70708
SuyeonShinnnn <suyeons5536@gmail.com>
2026-04-29 20:52:26 +0900

    Chore: /api prefix 제거 (#25)

--- src/apis/reportApi.ts
+++ src/apis/reportApi.ts
@@ -2,25 +2,25 @@ import type { FormInfo } from '../types/reportType';
 import axiosInstance from './axiosInstance';
 
 // 리포트 생성
-export const createReport = (formInfo: FormInfo) => {
-  return axiosInstance.post('/api/report/create', formInfo);
+export const createReport = (formInfo: FormInfo, userId: number) => {
+  return axiosInstance.post(`/report/create/${userId}`, formInfo);
 };
 
 // 리포트 조회
 export const fetchReportById = (reportId: number) => {
-  return axiosInstance.get(`/api/report/${reportId}`);
+  return axiosInstance.get(`/report/${reportId}`);
 };
 
 // 가격 분석 요청
 export const rentDealPrice = (reportId: number) => {
-  return axiosInstance.get('/api/report/rent-deal-analysis', {
+  return axiosInstance.get('/report/rent-deal-analysis', {
     params: { reportId },
   });
 };
 
 // 시세 추이
 export const fetchPriceHistory = (reportId: number) => {
-  return axiosInstance.get(`/api/report/price-history/${reportId}`);
+  return axiosInstance.get(`/report/price-history/${reportId}`);
 };
 
 // 권리 분석 요청
@@ -28,7 +28,7 @@ export const analyzeRightRisk = (reportId: number, address: string) => {
   if (!reportId) throw new Error('reportId가 필요합니다.');
   if (!address) throw new Error('주소가 필요합니다.');
 
-  return axiosInstance.post('/api/report/right-analysis', {
+  return axiosInstance.post('/report/right-analysis', {
     reportId: reportId,
     address: address.split('[')[0].trim(),
   });
@@ -36,29 +36,29 @@ export const analyzeRightRisk = (reportId: number, address: string) => {
 
 // 전세 사기 분석
 export const analysisFraud = (reportId: number) => {
-  return axiosInstance.get(`/api/report/fraud-analysis`, {
+  return axiosInstance.get(`/report/fraud-analysis`, {
     params: { reportId },
   });
 };
 
 // 등기부등본 조회
 export const fetchRightAnalysisResult = (reportId: number) => {
-  return axiosInstance.get(`/api/report/certificate/${reportId}`);
+  return axiosInstance.get(`/report/certificate/${reportId}`);
 };
 
 // 내 리포트 조회
 export const fetchMyReports = () => {
-  return axiosInstance.get('/api/my-page/reports');
+  return axiosInstance.get('/my-page/reports');
 };
 
 // 총 점수 업데이트
 export const updateTotalScore = (reportId: number, payload: any) => {
-  return axiosInstance.patch(`/api/report/score/${reportId}`, payload);
+  return axiosInstance.patch(`/report/score/${reportId}`, payload);
 };
 
 // 임대인 이름 위험 여부 조회
 export const fetchCautionByOwnerName = (ownerName: string) => {
-  return axiosInstance.get('/api/report/landlord/check', {
+  return axiosInstance.get('/report/landlord/check', {
     params: { ownerName },
   });
 };
