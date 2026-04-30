582365df29f35cd90995b86b3832bd137924595c
SuyeonShinnnn <suyeons5536@gmail.com>
2026-04-30 16:44:50 +0900

    Feat: 현재 위치 추적 기능 구현 (#35)

--- src/pages/building/BuildingPage.tsx
+++ src/pages/building/BuildingPage.tsx
@@ -1,12 +1,17 @@
-import styled from 'styled-components';
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
-import SearchBar from './components/SearchBar';
-import { useEffect, useState } from 'react';
-import type { Place } from '../../types/building';
-import BuildingInfoBar from './components/BuildingInfoBar';
-import { useLocation } from 'react-router-dom';
+import styled from "styled-components";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+import SearchBar from "./components/SearchBar";
+import { useEffect, useState } from "react";
+import type { Place } from "../../types/building";
+import BuildingInfoBar from "./components/BuildingInfoBar";
+import { useLocation } from "react-router-dom";
 
 const BuildingPage = () => {
+  const [center, setCenter] = useState({
+    lat: 37.579617,
+    lng: 126.977041,
+  });
+
   const [selected, setSelected] = useState<Place | null>(null);
   const [map, setMap] = useState<kakao.maps.Map | null>(null);
   const location = useLocation();
@@ -54,6 +59,21 @@ const BuildingPage = () => {
     handleSearch(address);
   }, [map, address]);
 
+  useEffect(() => {
+    if (!navigator.geolocation) return;
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude;
+
+      setCenter({ lat, lng });
+
+      if (map) {
+        map.panTo(new kakao.maps.LatLng(lat, lng));
+      }
+    });
+  }, [map]);
+
   return (
     <>
       <SearchBar
@@ -71,7 +91,7 @@ const BuildingPage = () => {
 
       <Container>
         <Map
-          center={{ lat: 33.450701, lng: 126.570667 }}
+          center={{ lat: center.lat, lng: center.lng }}
           className="map"
           level={3}
           onCreate={setMap}
