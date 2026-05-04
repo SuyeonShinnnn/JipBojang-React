f011b9475720831026ec75aceef20c9a3dcd799a
SuyeonShinnnn <suyeons5536@gmail.com>
2026-05-04 16:31:27 +0900

    Feat: react query 적용 및 로딩, 오류 상태 UI 구현 (#26)

--- src/pages/notification/NotiPage.tsx
+++ src/pages/notification/NotiPage.tsx
@@ -1,48 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import PropertyCard from './components/PropertyCard';
-import { getRegistedPropertyInfo } from '../../apis/notiApi';
-import WarningCardSection from './components/WarningCardSection';
-import type { PropertyDetail } from '../../types/notification';
-import { useAuthStore } from '../../stores/auth';
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import PropertyCard from "./components/PropertyCard";
+import { getRegistedPropertyInfo } from "../../apis/notiApi";
+import WarningCardSection from "./components/WarningCardSection";
+import { useAuthStore } from "../../stores/auth";
+import { useLocation } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import type { PropertyDetail } from "../../types/notification";
 
 const NotiPage: React.FC = () => {
   const location = useLocation();
   const targetId = location.state?.targetPropertyId;
   const userId = useAuthStore((state) => state.user.userId);
-  const [propertyInfo, setPropertyInfo] = useState<PropertyDetail[]>([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (!userId) return;
-
-      const data = await getRegistedPropertyInfo(Number(userId));
-      setPropertyInfo(data);
-    };
-    fetchData();
-  }, [userId]);
+  const { data, isPending, isError } = useQuery<PropertyDetail[]>({
+    queryKey: ["propertyInfo", userId],
+    queryFn: async () =>
+      await getRegistedPropertyInfo(Number(userId)).then((res) => res.data),
+    enabled: !!userId,
+  });
 
   const totalCards = 3;
+
+  const renderContent = () => {
+    if (isError) {
+      return <span>데이터를 불러오지 못했어요</span>;
+    }
+
+    if (isPending) {
+      return Array.from({ length: totalCards }).map((_, idx) => (
+        <div key={idx}>ㅇㅇㅇ</div>
+      ));
+    }
+
+    return Array.from({ length: totalCards }).map((_, idx) => {
+      const item = data?.[idx];
+
+      return (
+        <PropertyCard
+          key={idx}
+          propertyInfo={item}
+          isEmpty={!item}
+          autoOpenDetailModal={!!item && item.id === targetId}
+        />
+      );
+    });
+  };
 
   return (
     <Container>
       <h1>등기변동 알림 서비스</h1>
       <SubTitle>알림 설정된 부동산</SubTitle>
-      <PropertyCardWrapper>
-        {Array.from({ length: totalCards }).map((_, idx) => {
-          const item = propertyInfo[idx];
-
-          return (
-            <PropertyCard
-              key={idx}
-              propertyInfo={item}
-              isEmpty={!item}
-              autoOpenDetailModal={!!item && item.id === targetId}
-            />
-          );
-        })}
-      </PropertyCardWrapper>
+      <PropertyCardWrapper>{renderContent()}</PropertyCardWrapper>
       <WarningCardSection />
     </Container>
   );
