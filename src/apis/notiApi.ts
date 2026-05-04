ff6d2ca6fb4519f75c2d4cde6a9f39a7acfc6a37
SuyeonShinnnn <suyeons5536@gmail.com>
2026-05-04 12:57:28 +0900

    Fix: getRegistedPropertyDetail API 엔드포인트 수정 (#26)

--- src/apis/notiApi.ts
+++ src/apis/notiApi.ts
@@ -1,11 +1,8 @@
-import axios from 'axios';
 import axiosInstance from './axiosInstance';
 
 export const getRegistedPropertyInfo = async (id: number) => {
   try {
-    const response = await axiosInstance.get(
-      `/api/notification/registed/${id}`,
-    );
+    const response = await axiosInstance.get(`/notification/registed/${id}`);
     return response.data;
   } catch (e) {
     console.log('🚨오류 발생');
@@ -13,13 +10,10 @@ export const getRegistedPropertyInfo = async (id: number) => {
   }
 };
 
-export const getRegistedPropertyDetail = async (
-  userId: number,
-  commUniqueNo?: number,
-) => {
+export const getRegistedPropertyDetail = async (commUniqueNo?: number) => {
   try {
-    const response = await axios.get(
-      `/api/notification/regist/user/${userId}/uniqueNo/${commUniqueNo}`,
+    const response = await axiosInstance.get(
+      `/notification/regist/uniqueNo/${commUniqueNo}`,
     );
     console.log(response.data[0]);
     return response.data;
@@ -31,7 +25,7 @@ export const getRegistedPropertyDetail = async (
 
 export const searchAddress = async (keyword: string) => {
   try {
-    const res = await axios.post(`/api/codef/search`, { keyword });
+    const res = await axiosInstance.post(`/codef/search`, { keyword });
     console.log(res.data);
     return res.data;
   } catch (e) {
